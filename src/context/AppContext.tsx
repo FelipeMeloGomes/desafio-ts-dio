842c8d9e7b59d610d18b6b082e1f0c955d051225
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-16 00:06:41 -0300

    fix: removed unused imports

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,8 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import {
-    createLocalStorage,
-    getAllLocalStorage,
-} from "../services/Storage/storage";
+import { getAllLocalStorage } from "../services/Storage/storage";
 
 interface IAppContext {
     user: string;
@@ -21,7 +18,7 @@ export const AppContextProvider = ({ children }: any) => {
             const { login } = JSON.parse(storage);
             setIsLoggedIn(login);
         }
-    }, []);
+    }, [storage]);
 
     const user = "felipe";
 
